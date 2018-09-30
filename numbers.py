2895a9a3d3894b314e79dc47bbe05797342d0f86
sebastian-mc <s.martinezgd@gmail.com>
2018-09-29 19:46:30 -0500

    verde: numero de elementos, el minimo, el maximo y el promedio

--- numbers.py
+++ numbers.py
@@ -4,11 +4,14 @@ class Numbers:
         if cadena == "":
             return []
         numbers = cadena.split(",")
-        min = numbers[0]
-        max = numbers[0]
+        min = int(numbers[0])
+        max = int(numbers[0])
+        prom = 0
         for num in numbers:
+            num = int(num)
+            prom = prom + num
             if num < min:
                 min = num
             if num > max:
                 max = num
-        return [len(numbers), int(min), int(max)]
+        return [len(numbers), min, max, prom/len(numbers)]
