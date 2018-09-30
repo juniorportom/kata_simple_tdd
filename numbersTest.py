d4068a5aa92793316fed10a903b0f8f9979a55ff
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-09-29 19:24:26 -0500

    rojo: numero elementos y el minimo

--- numbersTest.py
+++ numbersTest.py
@@ -10,3 +10,7 @@ class NumbersTest(TestCase):
         self.assertEqual(Numbers().calcular('2')[0], 1, "Un elemento")
         self.assertEqual(Numbers().calcular('2,5'), [2], "Dos elementos")
         self.assertEqual(Numbers().calcular('2,5,7,8'), [4], "Varios elementos")
+
+    def test_calcular_numeroElementos_minimo(self):
+        self.assertEqual(Numbers().calcular('2,5,7,8'), [4, 2], "Varios elementos y el minimo")
+        self.assertEqual(Numbers().calcular('4,5,7,8,1,9'), [6, 1], "Varios elementos y el minimo")
