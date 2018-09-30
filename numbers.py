43a249f16a61978441cf5a6a0136e7356b29cf28
sebastian-mc <s.martinezgd@gmail.com>
2018-09-29 19:16:37 -0500

    verde: calcular dos o mas elementos

--- numbers.py
+++ numbers.py
@@ -5,5 +5,8 @@ class Numbers:
     def calcular(self, cadena):
         if cadena == "":
             return []
+        if "," in cadena:
+            numbers = cadena.split(",")
+            return [len(numbers)]
         else:
             return [1]
