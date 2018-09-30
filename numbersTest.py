6f933c37bb5a1e088b0ece36c5b44fb7ac316e37
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-09-29 19:06:18 -0500

    rojo: calcular un elemento

--- numbersTest.py
+++ numbersTest.py
@@ -5,3 +5,6 @@ from numbers import Numbers
 class NumbersTest(TestCase):
     def test_calcular(self):
         self.assertEqual(Numbers().calcular(''), [], "Cadena vacia")
+
+    def test_calcular_numeroElementos(self):
+        self.assertEqual(Numbers().calcular('2')[0], 1, "Un elemento")
