b850338b8551f58ccbca9106eeecb8139538ee54
Reinaldo Portocarrero <juniorportom@gmail.com>
2018-09-29 19:20:31 -0500

    azul: se elimina codigo repetido en calculo de elementos

--- numbers.py
+++ numbers.py
@@ -1,12 +1,7 @@
 
-
-
 class Numbers:
     def calcular(self, cadena):
         if cadena == "":
             return []
-        if "," in cadena:
-            numbers = cadena.split(",")
-            return [len(numbers)]
-        else:
-            return [1]
+        numbers = cadena.split(",")
+        return [len(numbers)]
